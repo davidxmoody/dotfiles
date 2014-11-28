9ab61202a77e17ae16544ae5e606a3882a89c3f8
David Moody <david@davidxmoody.com>
2014-11-28 17:38:20 +0000

    Word frequency script strips punctuation

--- bin/word_frequency.py
+++ bin/word_frequency.py
@@ -6,7 +6,9 @@ import fileinput
 word_tallies = Counter()
 
 for line in fileinput.input():
-    word_tallies.update(line.split())
+    words = line.split()
+    words = [word.lower().strip('.,!?[]()*{}-<>:;"\'') for word in words]
+    word_tallies.update(words)
 
 for word, count in word_tallies.most_common(2000):
     print(count, word)
