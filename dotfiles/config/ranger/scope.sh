fa548427b13ef1acef1a7db26c286156d6abce7b
David Moody <david@davidxmoody.com>
2016-06-27 16:17:26 +0100

    Disable ranger browser preview of html files

--- dotfiles/config/ranger/scope.sh
+++ dotfiles/config/ranger/scope.sh
@@ -72,24 +72,20 @@ case "$extension" in
         try bsdtar -lf "$path" && { dump | trim; exit 0; }
         exit 1;;
     rar)
-        try unrar -p- lt "$path" && { dump | trim; exit 0; } || exit 1;;
+        try unrar -p- lt "$path" && { dump | trim; exit 0; } || exit 1 ;;
     # PDF documents:
     pdf)
         try pdftotext -l 10 -nopgbrk -q "$path" - && \
-            { dump | trim | fmt -s -w $width; exit 0; } || exit 1;;
+            { dump | trim | fmt -s -w $width; exit 0; } || exit 1 ;;
     # BitTorrent Files
     torrent)
-        try transmission-show "$path" && { dump | trim; exit 5; } || exit 1;;
+        try transmission-show "$path" && { dump | trim; exit 5; } || exit 1 ;;
     # HTML Pages:
     htm|html|xhtml)
-        try w3m    -dump "$path" && { dump | trim | fmt -s -w $width; exit 4; }
-        try lynx   -dump "$path" && { dump | trim | fmt -s -w $width; exit 4; }
-        try elinks -dump "$path" && { dump | trim | fmt -s -w $width; exit 4; }
-        ;; # fall back to highlight/cat if the text browsers fail
+        fmt -s -w $width "$path" | trim; exit 4 ;;
     # Markdown files
     md|mkd|markdown)
-        fmt -s -w $width "$path" | trim; exit 4;
-        ;;
+        fmt -s -w $width "$path" | trim; exit 4 ;;
 esac
 
 case "$mimetype" in
