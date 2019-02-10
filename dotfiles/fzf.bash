c026d961fcb686c9e5bf9f3449bc2c6f88924be7
David Moody <david@davidxmoody.com>
2019-02-10 12:03:18 +0000

    Tweak manpath generation

--- dotfiles/fzf.bash
+++ dotfiles/fzf.bash
@@ -1,17 +1,3 @@
-# Setup fzf
-# ---------
-if [[ ! "$PATH" == *$HOME/.fzf/bin* ]]; then
-  export PATH="$PATH:$HOME/.fzf/bin"
-fi
-
-# Man path
-# --------
-if [[ ! "$MANPATH" == *$HOME/.fzf/man* && -d "$HOME/.fzf/man" ]]; then
-  export MANPATH="$MANPATH:$HOME/.fzf/man"
-fi
-
-complete -o bashdefault -o default -F _fzf_path_completion v
-
 # Key bindings
 # ------------
 source "$HOME/.fzf/shell/key-bindings.bash"
