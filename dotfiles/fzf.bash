15a264e5177e16a49f7716c6fa19c4f53c8d5219
David Moody <david@davidxmoody.com>
2019-02-10 12:05:43 +0000

    Move fzf config into main bashrc

--- dotfiles/fzf.bash
+++
@@ -1,8 +0,0 @@
-# Key bindings
-# ------------
-source "$HOME/.fzf/shell/key-bindings.bash"
-
-# Custom settings
-# ---------------
-export FZF_DEFAULT_COMMAND='rg --files --sortr=modified'
-export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
