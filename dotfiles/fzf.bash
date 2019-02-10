24f89feafe48e4600e9c5ed65fd6a795d5dea02c
David Moody <david@davidxmoody.com>
2019-02-10 09:32:42 +0000

    Add ripgrep config

--- dotfiles/fzf.bash
+++ dotfiles/fzf.bash
@@ -22,5 +22,5 @@ source "$HOME/.fzf/shell/key-bindings.bash"
 
 # Custom settings
 # ---------------
-export FZF_DEFAULT_COMMAND='rg --files'
+export FZF_DEFAULT_COMMAND='rg --files --sortr=modified'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
