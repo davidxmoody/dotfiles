aef4e71b9d99d378a0140d9c05b8e8a61796c984
David Moody <david@davidxmoody.com>
2019-02-20 15:31:35 +0000

    Add new Hammerspoon keybindings

--- dotfiles/hammerspoon/init.lua
+++ dotfiles/hammerspoon/init.lua
@@ -1,13 +1,27 @@
 function getTmuxWindow() return hs.window.find("tmux") end
 function getChromeWindow() return hs.window.find("Google Chrome") end
+function getSlackWindow() return hs.window.find("Slack") end
 
 function focusTmuxWindow() getTmuxWindow():focus() end
 function focusChromeWindow() getChromeWindow():focus() end
+function focusSlackWindow() getSlackWindow():focus() end
+
 function newChromeTab()
   focusChromeWindow()
   hs.eventtap.keyStroke({"cmd"}, "T")
 end
 
-hs.hotkey.bind({"cmd", "ctrl"}, "H", newChromeTab)
-hs.hotkey.bind({"cmd", "ctrl"}, "T", focusTmuxWindow)
-hs.hotkey.bind({"cmd", "ctrl"}, "N", focusChromeWindow)
+function openSlackChannel()
+  focusSlackWindow()
+  hs.eventtap.keyStroke({"cmd"}, "K")
+end
+
+hs.hotkey.bind({"cmd", "ctrl"}, "S", focusSlackWindow)
+hs.hotkey.bind({"cmd", "ctrl"}, "L", openSlackChannel)
+
+hs.hotkey.bind({"cmd", "ctrl"}, "H", focusTmuxWindow)
+
+hs.hotkey.bind({"cmd", "ctrl"}, "C", focusChromeWindow)
+hs.hotkey.bind({"cmd", "ctrl"}, "T", newChromeTab)
+
+hs.hotkey.bind({"cmd", "ctrl", "shift"}, "R", hs.reload)
