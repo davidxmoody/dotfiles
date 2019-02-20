97060253c08f1756ed692b4449b1fe4a63584e38
David Moody <david@davidxmoody.com>
2019-02-20 09:39:40 +0000

    Update hammerspoon config

--- dotfiles/hammerspoon/init.lua
+++ dotfiles/hammerspoon/init.lua
@@ -1,15 +1,13 @@
 function getTmuxWindow() return hs.window.find("tmux") end
 function getChromeWindow() return hs.window.find("Google Chrome") end
 
-hs.hotkey.bind({"alt", "ctrl"}, "T", function()
-  getTmuxWindow():focus()
-end)
+function focusTmuxWindow() getTmuxWindow():focus() end
+function focusChromeWindow() getChromeWindow():focus() end
+function newChromeTab()
+  focusChromeWindow()
+  hs.eventtap.keyStroke({"cmd"}, "T")
+end
 
-hs.hotkey.bind({"alt", "ctrl"}, "C", function()
-  getChromeWindow():focus()
-end)
-
-hs.hotkey.bind({"alt", "ctrl"}, "S", function()
-  getChromeWindow():focus()
-  hs.eventtap.keyStroke({"cmd"}, "3")
-end)
+hs.hotkey.bind({"cmd", "ctrl"}, "H", newChromeTab)
+hs.hotkey.bind({"cmd", "ctrl"}, "T", focusTmuxWindow)
+hs.hotkey.bind({"cmd", "ctrl"}, "N", focusChromeWindow)
