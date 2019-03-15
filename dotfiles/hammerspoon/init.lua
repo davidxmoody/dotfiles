ea48f668c9ec0be567f415eb95b33b264b5b68a4
David Moody <david@davidxmoody.com>
2019-03-15 09:45:00 +0000

    Try some more new Hammerspoon window functions

--- dotfiles/hammerspoon/init.lua
+++ dotfiles/hammerspoon/init.lua
@@ -6,15 +6,6 @@ function printTable(t)
   end
 end
 
-function focusTmuxWindow()
-  local win = hs.window.find("tmux")
-  if win == nil then
-    hs.alert.show("Could not find tmux window")
-  else
-    win:focus()
-  end
-end
-
 function focusIphoneSimulatorWindow()
   local win = hs.window.find("iPhone ")
   if win == nil then
@@ -22,29 +13,6 @@ function focusIphoneSimulatorWindow()
   else
     win:focus()
   end
-end
-
-function focusChromeWindow()
-  local win = hs.window.find("Google Chrome")
-  if win == nil then
-    hs.alert.show("Could not find Chrome window")
-  else
-    win:focus()
-  end
-end
-
--- function focusSlackWindow()
---   local win = hs.window.find("Slack - ")
---   if win == nil then
---     hs.alert.show("Could not find Slack window")
---   else
---     win:focus()
---   end
--- end
-
-function focusSlackWindow()
-  focusChromeWindow()
-  hs.eventtap.keyStroke({"cmd"}, "1")
 end
 
 function focusPosticoWindow()
@@ -83,23 +51,6 @@ function focusIphoneSimulatorOrPosticoWindow()
   end
 end
 
--- hs.hotkey.bind({"cmd", "ctrl"}, "B", focusPosticoWindow)
--- hs.hotkey.bind({"cmd", "ctrl"}, "M", focusIphoneSimulatorWindow)
-
--- hs.hotkey.bind({"cmd", "ctrl"}, "F", moveToOtherScreen)
-
--- hs.hotkey.bind({"cmd", "ctrl", "shift"}, "R", hs.reload)
-
--- Trying a different modifier key (to avoid having too many different modifiers).
--- These currently only work using my ErgoDox layout, would be good to get them
--- working on the standard Macbook Pro keyboard in Programmer Dvorak layout.
--- Would also be good to get auto keyboard switching working.
-
--- hs.hotkey.bind({"cmd", "shift"}, 28, openSlackChannel) -- "*"
--- hs.hotkey.bind({"cmd", "shift"}, 29, focusSlackWindow) -- ")"
--- hs.hotkey.bind({"cmd", "shift"}, 24, focusTmuxWindow) -- "+"
--- hs.hotkey.bind({"cmd"}, 30, focusChromeWindow) -- "]"
-
 -- TODO find a way to trigger these when external monitors are changed (or
 -- on some other event)
 -- hs.keycodes.setLayout("Programmer Dvorak")
@@ -107,27 +58,31 @@ end
 
 function openChromeThenSlack()
   local app = hs.application.applicationsForBundleID("com.google.Chrome")[1]
-  local win = app:mainWindow()
-
-  if win == nil then
-    -- No Chrome window: Can't continue
+  if app == nil then
     hs.alert.show("Could not find Chrome window")
-
-  elseif win ~= hs.window.focusedWindow() then
-    -- Chrome not focused: Focus it
-    win:focus()
-
-  elseif not string.match(win:title(), " | Candide Slack %- Google Chrome$") then
-    -- Chrome focused but Slack not: Focus Slack
-    hs.eventtap.keyStroke({"cmd"}, "1")
-
-  else
-    -- Chrome and Slack focused: Open Slack channel
-    hs.eventtap.keyStroke({"cmd"}, "K")
+    return
   end
+
+  local win = app:mainWindow()
+  if win == nil then
+    hs.alert.show("Could not find Chrome window")
+    return
+  end
+
+  if win ~= hs.window.focusedWindow() then
+    win:focus()
+    return
+  end
+
+  if not string.match(win:title(), " | Candide Slack %- Google Chrome$") then
+    hs.eventtap.keyStroke({"cmd"}, "1") -- Focus first tab (Slack)
+    return
+  end
+
+  hs.eventtap.keyStroke({"cmd"}, "K") -- Open Slack channel chooser
 end
 
-function toggleItermFocus()
+function focusIterm()
   local app = hs.application.applicationsForBundleID("com.googlecode.iterm2")[1]
   local win = app:mainWindow()
 
@@ -137,19 +92,49 @@ function toggleItermFocus()
     win:focus()
   else
     -- Do nothing
-    -- app:hide()
   end
+end
+
+function focusSimulatorAndChrome()
+  local app = hs.application.applicationsForBundleID("com.apple.iphonesimulator")[1]
+  if app == nil then
+    hs.alert.show("No simulator application")
+    return
+  end
+
+  local win = app:mainWindow()
+  if win == nil then
+    hs.alert.show("No simulator window")
+    return
+  end
+
+  local chrome_app = hs.application.applicationsForBundleID("com.google.Chrome")[1]
+  if chrome_app == nil then
+    hs.alert.show("Could not find Chrome window")
+    return
+  end
+
+  local chrome_win = chrome_app:mainWindow()
+  if chrome_win == nil then
+    hs.alert.show("Could not find Chrome window")
+    return
+  end
+
+  chrome_win:focus()
+  win:focus()
 end
 
 mod = {"cmd", "ctrl"}
 
 hs.hotkey.bind(mod, "Z", openChromeThenSlack) -- Mouse
 
-hs.hotkey.bind(mod, "N", openChromeThenSlack)
-hs.hotkey.bind(mod, "T", toggleItermFocus)
-hs.hotkey.bind(mod, "H", openChromeThenSlack)
-hs.hotkey.bind(mod, "C", focusIphoneSimulatorOrPosticoWindow)
-hs.hotkey.bind(mod, "W", focusIphoneSimulatorOrPosticoWindow)
+hs.hotkey.bind(mod, "T", focusIterm) -- Left bottom
+
+hs.hotkey.bind(mod, "N", openChromeThenSlack) -- Right bottom
+hs.hotkey.bind(mod, "H", focusSimulatorAndChrome) -- Right middle
+hs.hotkey.bind(mod, "C", focusIphoneSimulatorOrPosticoWindow) -- Right top
+hs.hotkey.bind(mod, "W", focusIphoneSimulatorOrPosticoWindow) -- Right top-right
+
 hs.hotkey.bind({"cmd", "ctrl", "shift"}, "R", hs.reload)
 
 hs.alert.show("Config loaded")
