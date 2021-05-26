f8cfcb249048013ecfda10ac48151174f1196859
David Moody <david@davidxmoody.com>
2021-05-26 07:59:29 +0100

    Fix highlights getting overridden

--- dotfiles/config/nvim/lua/colorscheme.lua
+++ dotfiles/config/nvim/lua/colorscheme.lua
@@ -1,13 +1,18 @@
+local function highlight(hi)
+  vim.cmd("autocmd ColorScheme * highlight " .. hi)
+end
+
 vim.g["nightflyUnderlineMatchParen"] = 1
 
 vim.cmd("colorscheme nightfly")
 
-vim.cmd("hi Pmenu ctermfg=0 ctermbg=13 guifg=fg guibg=#1d3b53")
+highlight("CustomModifiedFlag guibg=Red guifg=White")
+highlight("Pmenu ctermfg=0 ctermbg=13 guifg=fg guibg=#1d3b53")
 
 -- For focused windows, use the 'default' background color (from tmux). This
 -- means the current Vim window will be highlighted only if the tmux pane that
 -- vim is running in is also currently active.
-vim.cmd("highlight Normal guibg=NONE")
+highlight("Normal guibg=NONE")
 
 -- Unfocused background color duplicated in tmux config
-vim.cmd("highlight NormalNC guibg=#000a13")
+highlight("NormalNC guibg=#000a13")
--- dotfiles/config/nvim/lua/statusline.lua
+++ dotfiles/config/nvim/lua/statusline.lua
@@ -1,5 +1,3 @@
-vim.cmd("highlight CustomModifiedFlag guibg=Red guifg=White")
-
 vim.o.statusline = table.concat({
   "%<%f", " %h%q%r", "%#CustomModifiedFlag#%m%*", "%= ",
   "%03.(%c%) %07.(%l/%L%)", "%( %y%)",
