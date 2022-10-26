095ffec8aa8edc93afe71d3749d6de78729b3a30
David Moody <david@davidxmoody.com>
2022-10-26 15:47:34 +0100

    Remove custom keymap function

--- dotfiles/config/nvim/lua/keymapping.lua
+++ dotfiles/config/nvim/lua/keymapping.lua
@@ -1,212 +1,200 @@
-local function map(mode, lhs, rhs, options)
-  vim.api.nvim_set_keymap(mode, lhs, rhs, vim.tbl_extend("force", {
-    noremap = true,
-    silent = true,
-  }, options or {}))
-end
-
-local function map_plug(mode, lhs, rhs)
-  map(mode, lhs, rhs, {noremap = false})
-end
-
 -- Leader key
 
-map("", "<Space>", "<Nop>")
 vim.g.mapleader = " "
+vim.keymap.set("", "<Space>", "<Nop>")
 
 -- Dvorak navigation
 
-map("", "t", "j")
-map("", "n", "k")
-map("", "s", "l")
+vim.keymap.set("", "t", "j")
+vim.keymap.set("", "n", "k")
+vim.keymap.set("", "s", "l")
 
-map("", "T", "")
-map("", "N", "")
-map("", "H", "0")
-map("", "S", "$")
+vim.keymap.set("", "T", "")
+vim.keymap.set("", "N", "")
+vim.keymap.set("", "H", "0")
+vim.keymap.set("", "S", "$")
 
-map("", "gt", "gj")
-map("", "gn", "gk")
+vim.keymap.set("", "gt", "gj")
+vim.keymap.set("", "gn", "gk")
 
-map("i", "+", "<Right><Esc>")
-map("v", "+", "<Esc><Esc>")
-map("", "+", ":update<CR>")
+vim.keymap.set("i", "+", "<Right><Esc>")
+vim.keymap.set("v", "+", "<Esc><Esc>")
+vim.keymap.set("", "+", ":update<CR>")
 
 -- Consistent text navigation
 
-map("", "<M-B>", "b")
-map("", "<M-F>", "w")
+vim.keymap.set("", "<M-B>", "b")
+vim.keymap.set("", "<M-F>", "w")
 
-map("i", "<M-B>", "<S-Left>")
-map("i", "<M-F>", "<S-Right>")
-map("i", "<M-BS>", "<C-w>")
-map("i", "<C-D>", "<Del>")
+vim.keymap.set("i", "<M-B>", "<S-Left>")
+vim.keymap.set("i", "<M-F>", "<S-Right>")
+vim.keymap.set("i", "<M-BS>", "<C-w>")
+vim.keymap.set("i", "<C-D>", "<Del>")
 
-map("c", "<C-A>", "<Home>", {silent = false})
-map("c", "<M-B>", "<S-Left>", {silent = false})
-map("c", "<M-F>", "<S-Right>", {silent = false})
-map("c", "<M-BS>", "<C-w>", {silent = false})
-map("c", "<C-D>", "<Del>", {silent = false})
+vim.keymap.set("c", "<C-A>", "<Home>")
+vim.keymap.set("c", "<M-B>", "<S-Left>")
+vim.keymap.set("c", "<M-F>", "<S-Right>")
+vim.keymap.set("c", "<M-BS>", "<C-w>")
+vim.keymap.set("c", "<C-D>", "<Del>")
 
 -- Joining
 
-map("", "k", "J")
-map("", "K", "gJ")
+vim.keymap.set("", "k", "J")
+vim.keymap.set("", "K", "gJ")
 
 -- Undo/redo
 
-map("", "U", "<C-R>")
-map("", "<C-R>", "<Nop>")
+vim.keymap.set("", "U", "<C-R>")
+vim.keymap.set("", "<C-R>", "<Nop>")
 
 -- Terminal
 
-map("t", "<Esc>", "<C-\\><C-n>")
+vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
 
 -- FZF
 
-map("", "<leader>e", ":Files<CR>")
-map("", "<leader>E", ":Buffers<CR>")
-map("", "<leader>o", ":GFiles?<CR>")
-map("", "<leader>a", ":Rg<space>", {silent = false})
-map("", "<leader>A", ":Rg<space><Up><CR>")
-map("", "<leader>i", ":Rg<space>/<C-R>=fnameescape(expand('%:t:r'))<CR><CR>")
-map("", "<leader>B", ":History<CR>")
+vim.keymap.set("", "<leader>e", ":Files<CR>")
+vim.keymap.set("", "<leader>E", ":Buffers<CR>")
+vim.keymap.set("", "<leader>o", ":GFiles?<CR>")
+vim.keymap.set("", "<leader>a", ":Rg<space>")
+vim.keymap.set("", "<leader>A", ":Rg<space><Up><CR>")
+vim.keymap.set("", "<leader>i", ":Rg<space>/<C-R>=fnameescape(expand('%:t:r'))<CR><CR>")
+vim.keymap.set("", "<leader>B", ":History<CR>")
 
 -- File navigation
 
-map("", "<leader>u", "ZZ")
-map("", "<leader>b", "<C-^>")
-map("", "<C-d>", ":q<CR>")
+vim.keymap.set("", "<leader>u", "ZZ")
+vim.keymap.set("", "<leader>b", "<C-^>")
+vim.keymap.set("", "<C-d>", ":q<CR>")
 
-map("", "-", ":NvimTreeFindFile<CR>")
-map("", "g-", ":NvimTreeToggle<CR>")
+vim.keymap.set("", "-", ":NvimTreeFindFile<CR>")
+vim.keymap.set("", "g-", ":NvimTreeToggle<CR>")
 
 -- Selection
 
-map_plug("", "<leader>v", "Vii")
-map("", "<leader>V", "ggVG")
+vim.keymap.set("", "<leader>v", "Vii", {remap=true})
+vim.keymap.set("", "<leader>V", "ggVG")
 
 -- Jumping
 
-map_plug("", "o", "<Plug>(easymotion-overwin-f2)")
-map_plug("", "O", "<Plug>(easymotion-overwin-f2)")
-map_plug("", "f", "<Plug>Sneak_f")
-map_plug("", "F", "<Plug>Sneak_F")
-map_plug("", "l", "<Plug>Sneak_t")
-map_plug("", "L", "<Plug>Sneak_T")
-map_plug("", "(", "<Plug>Sneak_,")
-map_plug("", ")", "<Plug>Sneak_;")
+vim.keymap.set("", "o", "<Plug>(easymotion-overwin-f2)")
+vim.keymap.set("", "f", "<Plug>Sneak_f")
+vim.keymap.set("", "F", "<Plug>Sneak_F")
+vim.keymap.set("", "l", "<Plug>Sneak_t")
+vim.keymap.set("", "L", "<Plug>Sneak_T")
+vim.keymap.set("", "(", "<Plug>Sneak_,")
+vim.keymap.set("", ")", "<Plug>Sneak_;")
 
-map("n", ";", "g;")
+vim.keymap.set("n", ";", "g;")
 
 -- Search
 
-map("", "j", "n")
-map("", "J", "N")
-map("", "gj", "gn")
-map("", "gJ", "gN")
+vim.keymap.set("", "j", "n")
+vim.keymap.set("", "J", "N")
+vim.keymap.set("", "gj", "gn")
+vim.keymap.set("", "gJ", "gN")
 
-map("n", "*", "/\\C\\<<C-R><C-W>\\><CR>")
-map("n", "<leader>*", ":Rg <C-R><C-W><CR>")
-map("x", "*", "\"zy/<C-R><C-R>z<CR>")
-map("x", "<leader>*", "\"zy:Rg <C-R><C-R>z<CR>")
+vim.keymap.set("n", "*", "/\\C\\<<C-R><C-W>\\><CR>")
+vim.keymap.set("n", "<leader>*", ":Rg <C-R><C-W><CR>")
+vim.keymap.set("x", "*", "\"zy/<C-R><C-R>z<CR>")
+vim.keymap.set("x", "<leader>*", "\"zy:Rg <C-R><C-R>z<CR>")
 
-map("n", "gx", "/export<CR>")
+vim.keymap.set("n", "gx", "/export<CR>")
 
 -- Entering insertion
 
-map("", ",", "A")
-map("", "<leader>,", "GA")
-map("", "<CR>", "o")
-map("", "g<CR>", "O")
+vim.keymap.set("", ",", "A")
+vim.keymap.set("", "<leader>,", "GA")
+vim.keymap.set("", "<CR>", "o")
+vim.keymap.set("", "g<CR>", "O")
 
 -- Repeat pasting
 
-map("x", "gp", "\"0p")
-map("x", "gP", "\"0P")
-map("n", "gp", "V\"0p")
-map("n", "gP", "V\"0p")
+vim.keymap.set("x", "gp", "\"0p")
+vim.keymap.set("x", "gP", "\"0P")
+vim.keymap.set("n", "gp", "V\"0p")
+vim.keymap.set("n", "gP", "V\"0p")
 
 -- Repeat operator
 
-map("x", ".", ":norm.<CR>")
-map_plug("", "<leader>.", "/\\C\\<\"\\><CR>.")
+vim.keymap.set("x", ".", ":norm.<CR>")
+vim.keymap.set("", "<leader>.", "/\\C\\<\"\\><CR>.")
 
 -- Window splitting
 
-map("", "<leader>h", ":aboveleft vsplit<CR>")
-map("", "<leader>t", ":belowright split<CR>")
-map("", "<leader>n", ":aboveleft split<CR>")
-map("", "<leader>s", ":belowright vsplit<CR>")
+vim.keymap.set("", "<leader>h", ":aboveleft vsplit<CR>")
+vim.keymap.set("", "<leader>t", ":belowright split<CR>")
+vim.keymap.set("", "<leader>n", ":aboveleft split<CR>")
+vim.keymap.set("", "<leader>s", ":belowright vsplit<CR>")
 
 -- Tmux window navigation
 
-map("n", "<C-H>", ":TmuxNavigateLeft<CR>")
-map("n", "<C-T>", ":TmuxNavigateDown<CR>")
-map("n", "<C-N>", ":TmuxNavigateUp<CR>")
-map("n", "<C-S>", ":TmuxNavigateRight<CR>")
+vim.keymap.set("n", "<C-H>", ":TmuxNavigateLeft<CR>")
+vim.keymap.set("n", "<C-T>", ":TmuxNavigateDown<CR>")
+vim.keymap.set("n", "<C-N>", ":TmuxNavigateUp<CR>")
+vim.keymap.set("n", "<C-S>", ":TmuxNavigateRight<CR>")
 
-map("v", "<C-H>", "<Esc><Esc>:TmuxNavigateLeft<CR>")
-map("v", "<C-T>", "<Esc><Esc>:TmuxNavigateDown<CR>")
-map("v", "<C-N>", "<Esc><Esc>:TmuxNavigateUp<CR>")
-map("v", "<C-S>", "<Esc><Esc>:TmuxNavigateRight<CR>")
+vim.keymap.set("v", "<C-H>", "<Esc><Esc>:TmuxNavigateLeft<CR>")
+vim.keymap.set("v", "<C-T>", "<Esc><Esc>:TmuxNavigateDown<CR>")
+vim.keymap.set("v", "<C-N>", "<Esc><Esc>:TmuxNavigateUp<CR>")
+vim.keymap.set("v", "<C-S>", "<Esc><Esc>:TmuxNavigateRight<CR>")
 
-map("i", "<C-H>", "<Right><Esc>:TmuxNavigateLeft<CR>")
-map("i", "<C-T>", "<Right><Esc>:TmuxNavigateDown<CR>")
-map("i", "<C-N>", "<Right><Esc>:TmuxNavigateUp<CR>")
-map("i", "<C-S>", "<Right><Esc>:TmuxNavigateRight<CR>")
+vim.keymap.set("i", "<C-H>", "<Right><Esc>:TmuxNavigateLeft<CR>")
+vim.keymap.set("i", "<C-T>", "<Right><Esc>:TmuxNavigateDown<CR>")
+vim.keymap.set("i", "<C-N>", "<Right><Esc>:TmuxNavigateUp<CR>")
+vim.keymap.set("i", "<C-S>", "<Right><Esc>:TmuxNavigateRight<CR>")
 
-map("t", "<C-H>", "<C-\\><C-N>:TmuxNavigateLeft<CR>")
-map("t", "<C-T>", "<C-\\><C-N>:TmuxNavigateDown<CR>")
-map("t", "<C-N>", "<C-\\><C-N>:TmuxNavigateUp<CR>")
-map("t", "<C-S>", "<C-\\><C-N>:TmuxNavigateRight<CR>")
+vim.keymap.set("t", "<C-H>", "<C-\\><C-N>:TmuxNavigateLeft<CR>")
+vim.keymap.set("t", "<C-T>", "<C-\\><C-N>:TmuxNavigateDown<CR>")
+vim.keymap.set("t", "<C-N>", "<C-\\><C-N>:TmuxNavigateUp<CR>")
+vim.keymap.set("t", "<C-S>", "<C-\\><C-N>:TmuxNavigateRight<CR>")
 
-map("c", "<C-H>", "<Nop>")
-map("c", "<C-T>", "<Nop>")
-map("c", "<C-N>", "<Nop>")
-map("c", "<C-S>", "<Nop>")
+vim.keymap.set("c", "<C-H>", "<Nop>")
+vim.keymap.set("c", "<C-T>", "<Nop>")
+vim.keymap.set("c", "<C-N>", "<Nop>")
+vim.keymap.set("c", "<C-S>", "<Nop>")
 
 -- Git
 
-map("", "<leader>gb", ":Git blame<CR>")
-map("", "<leader>gr", ":Gread<CR>")
-map("", "<leader>gw", ":Gwrite<CR>")
+vim.keymap.set("", "<leader>gb", ":Git blame<CR>")
+vim.keymap.set("", "<leader>gr", ":Gread<CR>")
+vim.keymap.set("", "<leader>gw", ":Gwrite<CR>")
 
-map_plug("", "<leader>ga", "<Plug>(GitGutterStageHunk)")
-map_plug("", "<leader>gu", "<Plug>(GitGutterUndoHunk)")
-map_plug("", "]h", "<Plug>(GitGutterNextHunk)")
-map_plug("", "[h", "<Plug>(GitGutterPrevHunk)")
-map_plug("o", "ih", "<Plug>(GitGutterTextObjectInnerPending)")
-map_plug("o", "ah", "<Plug>(GitGutterTextObjectOuterPending)")
-map_plug("x", "ih", "<Plug>(GitGutterTextObjectInnerVisual)")
-map_plug("x", "ah", "<Plug>(GitGutterTextObjectOuterVisual)")
+vim.keymap.set("", "<leader>ga", "<Plug>(GitGutterStageHunk)")
+vim.keymap.set("", "<leader>gu", "<Plug>(GitGutterUndoHunk)")
+vim.keymap.set("", "]h", "<Plug>(GitGutterNextHunk)")
+vim.keymap.set("", "[h", "<Plug>(GitGutterPrevHunk)")
+vim.keymap.set("o", "ih", "<Plug>(GitGutterTextObjectInnerPending)")
+vim.keymap.set("o", "ah", "<Plug>(GitGutterTextObjectOuterPending)")
+vim.keymap.set("x", "ih", "<Plug>(GitGutterTextObjectInnerVisual)")
+vim.keymap.set("x", "ah", "<Plug>(GitGutterTextObjectOuterVisual)")
 
 -- Toggle settings
 
-map("", "<leader>w", ":set wrap!<CR>")
-map("", "<leader>W", ":set colorcolumn=80<CR>")
-map("", "<leader>/", ":nohlsearch<CR>")
+vim.keymap.set("", "<leader>w", ":set wrap!<CR>")
+vim.keymap.set("", "<leader>W", ":set colorcolumn=80<CR>")
+vim.keymap.set("", "<leader>/", ":nohlsearch<CR>")
 
 -- Slime
 
-map_plug("x", "<leader>p", "<Plug>SlimeRegionSend")
-map_plug("n", "<leader>p", "<Plug>SlimeParagraphSend")
-map_plug("n", "<leader>P", "<Plug>SlimeConfig")
+vim.keymap.set("x", "<leader>p", "<Plug>SlimeRegionSend")
+vim.keymap.set("n", "<leader>p", "<Plug>SlimeParagraphSend")
+vim.keymap.set("n", "<leader>P", "<Plug>SlimeConfig")
 
 -- Refactoring
 
-map("v", "<C-K>", [[:s/^\( *\)\([^:]\+\):.*$/\1"\2",/<CR>]])
+vim.keymap.set("v", "<C-K>", [[:s/^\( *\)\([^:]\+\):.*$/\1"\2",/<CR>]])
 
-map("n", "<C-K>", "cl<CR><Esc>lf<Space>")
+vim.keymap.set("n", "<C-K>", "cl<CR><Esc>lf<Space>")
 
-map("", "ga", ":.Subvert/{true,false}/{false,true}/g<CR>:nohlsearch<CR>")
+vim.keymap.set("", "ga", ":.Subvert/{true,false}/{false,true}/g<CR>:nohlsearch<CR>")
 
-map("n", "<leader>r", [[:%s/\<<C-R><C-W>\>/<C-R><C-W>/g<Left><Left>]], { silent = false })
-map("x", "<leader>r", [["zy:%s/\<<C-R><C-R>z\>/<C-R><C-R>z/g<Left><Left>]], { silent = false })
+vim.keymap.set("n", "<leader>r", [[:%s/\<<C-R><C-W>\>/<C-R><C-W>/g<Left><Left>]])
+vim.keymap.set("x", "<leader>r", [["zy:%s/\<<C-R><C-R>z\>/<C-R><C-R>z/g<Left><Left>]])
 
-map("", "<leader>f", ":Neoformat<CR>")
+vim.keymap.set("", "<leader>f", ":Neoformat<CR>")
 
-map_plug("x", "<leader>x",
+vim.keymap.set("x", "<leader>x",
   [["lc<C-R>=substitute(system('node -p', @l), '\n\+$', '', '')<CR><ESC>]])
 
 vim.cmd([[
@@ -215,15 +203,4 @@ vim.cmd([[
     execute ":'<,'>normal @".nr2char(getchar())
   endfunction
 ]])
-map("x", "@", ":<C-u>call ExecuteMacroOverVisualRange()<CR>")
-
-vim.cmd([[
-  function! FillLine(str)
-    .s/\s*$//
-    let reps = (79 - col('$')) / len(a:str)
-    if reps > 0
-      .s/$/\=(' '.repeat(a:str, reps))/
-    endif
-  endfunction
-]])
-map("n", "<leader>~", ":call FillLine(\"~\")<CR>")
+vim.keymap.set("x", "@", ":<C-u>call ExecuteMacroOverVisualRange()<CR>")
