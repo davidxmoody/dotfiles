cc5dd685f86ac109f1a76c675c7b9726f0f427bf
David Moody <david@davidxmoody.com>
2022-10-27 10:17:27 +0100

    Fix deprecated lsp mappings

--- dotfiles/config/nvim/lua/lsp.lua
+++ dotfiles/config/nvim/lua/lsp.lua
@@ -20,9 +20,9 @@ local on_attach = function(client, bufnr)
   buf_set_keymap("n", "gr", "<Cmd>lua vim.lsp.buf.references()<CR>", opts)
   buf_set_keymap("n", "gR", "<Cmd>lua vim.lsp.buf.rename()<CR>", opts)
   buf_set_keymap("n", "gS",
-    "<Cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>", opts)
+    "<Cmd>lua vim.diagnostic.show_line_diagnostics()<CR>", opts)
   buf_set_keymap("n", "gs", "<Cmd>lua vim.lsp.buf.code_action()<CR>", opts)
-  buf_set_keymap("n", "_", "<Cmd>lua vim.lsp.diagnostic.goto_next()<CR>", opts)
+  buf_set_keymap("n", "_", "<Cmd>lua vim.diagnostic.goto_next()<CR>", opts)
 end
 
 -- local servers = {"denols"}
