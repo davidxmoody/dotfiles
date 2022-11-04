55da1a085be4021be440fd690545b956a8034e1f
David Moody <david@davidxmoody.com>
2022-11-04 15:27:59 +0000

    Fix lsp keybinding

--- dotfiles/config/nvim/lua/lsp.lua
+++ dotfiles/config/nvim/lua/lsp.lua
@@ -16,12 +16,10 @@ local on_attach = function(client, bufnr)
   buf_set_keymap("n", "gd", "<Cmd>lua vim.lsp.buf.definition()<CR>", opts)
   buf_set_keymap("n", "gD", "<Cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
   buf_set_keymap("n", "gl", "<Cmd>lua vim.lsp.buf.hover()<CR>", opts)
-  buf_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
   buf_set_keymap("n", "gr", "<Cmd>lua vim.lsp.buf.references()<CR>", opts)
   buf_set_keymap("n", "gR", "<Cmd>lua vim.lsp.buf.rename()<CR>", opts)
-  buf_set_keymap("n", "gS",
-    "<Cmd>lua vim.diagnostic.show_line_diagnostics()<CR>", opts)
   buf_set_keymap("n", "gs", "<Cmd>lua vim.lsp.buf.code_action()<CR>", opts)
+  buf_set_keymap("n", "gS", "<Cmd>lua vim.diagnostic.open_float()<CR>", opts)
   buf_set_keymap("n", "_", "<Cmd>lua vim.diagnostic.goto_next()<CR>", opts)
 end
 
