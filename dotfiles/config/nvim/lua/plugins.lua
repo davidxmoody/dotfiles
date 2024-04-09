213b37826ad018f6d612ee507c9d01febe85a349
David Moody <david@davidxmoody.com>
2024-04-09 14:44:43 +0100

    Remove unused hipatterns

--- dotfiles/config/nvim/lua/plugins.lua
+++ dotfiles/config/nvim/lua/plugins.lua
@@ -76,22 +76,6 @@ require("lazy").setup({
 
       require("mini.hipatterns").setup({
         highlighters = {
-          fixme = {
-            pattern = "%f[%w]()FIXME()%f[%W]",
-            group = "MiniHipatternsFixme",
-          },
-          hack = {
-            pattern = "%f[%w]()HACK()%f[%W]",
-            group = "MiniHipatternsHack",
-          },
-          todo = {
-            pattern = "%f[%w]()TODO()%f[%W]",
-            group = "MiniHipatternsTodo",
-          },
-          note = {
-            pattern = "%f[%w]()NOTE()%f[%W]",
-            group = "MiniHipatternsNote",
-          },
           hex_color = require("mini.hipatterns").gen_highlighter.hex_color(),
           cells = {
             pattern = "^# %%%%.*",
